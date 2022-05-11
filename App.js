c56fda0fffa69c5fb5f674942d652230bf5d0d98
RagelAlexis <alexis.rageldu76@hotmail.fr>
2022-05-11 09:30:03 +0200

    Exercice fait

--- App.js
+++ App.js
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
 
-export default function App() {
+const Exo1 = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <View style={{backgroundColor:'green', flex:2}}></View>
+      <View style={{backgroundColor:'red', flex:1}}></View>
+      <View style={{backgroundColor:'blue', flex:3}}></View>
     </View>
-  );
-}
+  )
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Exo2 = () => {
+  return (
+    <View style={{flex:1, flexDirection:'row'}}>
+      <View style={{backgroundColor:'green', flex:3}}></View>
+      <View style={{backgroundColor:'red', flex:2}}></View>
+      <View style={{backgroundColor:'blue', flex:1}}></View>
+    </View>
+  )
+};
+
+const Exo3 = () => {
+  return (
+    <View style={{flex:1}}>
+
+      <View style={{flex:1, flexDirection:'row'}}>
+        <View style={{backgroundColor:'green', flex:3}}></View>
+        <View style={{backgroundColor:'red', flex:2}}></View>
+        <View style={{backgroundColor:'blue', flex:1}}></View>
+      </View>
+
+        <View style={{flex:1}}>
+          <View style={{backgroundColor:'purple', flex:3}}></View>
+          <View style={{backgroundColor:'yellow', flex:2}}></View>
+          <View style={{backgroundColor:'pink', flex:1}}></View>
+        </View>
+
+    </View>
+  )
+};
+
+const Exo4 = () => {
+  return (
+    <View style={{flex:1}}>
+
+        <View style={{flex:1, flexDirection:'row'}}>
+
+          <View style={{flex:1, flexDirection:'row'}}>
+            <View style={{backgroundColor:'blue', flex:1}}></View>
+            <View style={{backgroundColor:'red', flex:2}}></View>
+          </View>
+
+          <View style={{flex:1}}>
+            <View style={{backgroundColor:'purple', flex:3}}></View>
+            <View style={{backgroundColor:'yellow', flex:2}}></View>
+            <View style={{backgroundColor:'pink', flex:1}}></View>
+          </View>
+
+      </View>  
+
+      <View style={{backgroundColor:'gray', flex:1}}></View>
+
+    </View>
+  )
+};
+
+const Exo5 = () => {
+  return (
+    <View style={{flex:1}}>
+
+        <View style={{flex:1, flexDirection:'row'}}>
+
+          <View style={{flex:3}}>
+            <View style={{backgroundColor:'purple', flex:3}}></View>
+            <View style={{backgroundColor:'yellow', flex:2}}></View>
+            <View style={{backgroundColor:'pink', flex:1}}></View>
+          </View>
+
+          <View style={{flex:1, flexDirection:'row'}}>
+            <View style={{backgroundColor:'red', flex:2}}></View>
+            <View style={{backgroundColor:'blue', flex:1}}></View>
+          </View>
+
+          <View style={{flex:1}}>
+            <View style={{backgroundColor:'cyan', flex:3}}></View>
+            <View style={{backgroundColor:'magenta', flex:2}}></View>
+          </View>
+
+        </View>
+
+        <View style={{backgroundColor:'gray', flex:1}}></View>
+
+    </View>
+  )
+};
+
+export default Exo1
